871aaaefbb444ad931be5560f9a97e22d49ddc21
tabreturn <github@tabreturn.com>
2020-07-25 23:36:37 +1200

    - add whitespacing/lines

--- generate.py
+++ generate.py
@@ -5,19 +5,24 @@ from jinja2 import Environment, FileSystemLoader
 from markdown2 import markdown
 
 # create _site directory
+
 if not os.path.exists('_site'):
     os.makedirs('_site')
 
 examples = {}
 
 # load examples markdown into examples dictionary
+
 for eg in os.listdir('examples'):
+
     if eg.endswith('.md'):
+
         with open(os.path.join('examples', eg), 'r') as file:
             extras = ['metadata', 'fenced-code-blocks']
             examples[eg] = markdown(file.read(), extras=extras)
 
 # load templates
+
 env = Environment(loader=FileSystemLoader('templates'))
 
 templates = {
@@ -26,12 +31,15 @@ templates = {
 }
 
 # generate landing page
+
 metadata = [examples[eg].metadata for eg in examples]
 index_html = templates['index'].render(contents=metadata)
+
 with open('_site/index.html', 'w') as file:
     file.write(index_html)
 
 # generate example pages
+
 for eg in examples:
     metadata = examples[eg].metadata
     contents = {
@@ -42,14 +50,18 @@ for eg in examples:
     example_html = templates['example'].render(contents=contents)
     path = '_site/{}.html'.format(metadata['title'])
     os.makedirs(os.path.dirname(path), exist_ok=True)
+
     with open(path, 'w') as file:
         file.write(example_html)
 
 asset_paths = ['examples', 'templates']
 
 # copy static assets into _site directory
+
 for path in asset_paths:
+
     for asset in os.listdir(path):
+
         if asset.endswith(('.css', '.js', '.png')):
             site_path = os.path.join(path, asset)
             shutil.copy(site_path, os.path.join('_site', asset))
