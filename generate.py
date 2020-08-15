32082e51e1f69a22866cc6294645ceb725f7c2de
tabreturn <github@tabreturn.com>
2020-08-15 20:25:28 +1200

    - remove unused prepend_line() definition

--- generate.py
+++ generate.py
@@ -88,21 +88,6 @@ def add_draw_and_setup_functions(code):
 
     return result
 
-def prepend_line(file_name, line):
-    """ Insert given string as a new line at the beginning of a file """
-    # define name of temporary dummy file
-    dummy_file = file_name + '.bak'
-    # open original file in read mode and dummy file in write mode
-    with open(file_name, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
-        # Write given line to the dummy file
-        write_obj.write(line + '\n')
-        # Read lines from original file one by one and append them to the dummy file
-        for line in read_obj:
-            write_obj.write(line)
-    # remove original file
-    os.remove(file_name)
-    # Rename dummy file as the original file
-    os.rename(dummy_file, file_name)
 
 # transcribe sketches using pyp5js
 
